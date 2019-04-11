5fda3b65b29f0c2d8a7320662e6901c354fb8b0c
Vicky <vikasd.javarnd@gmail.com>
2019-04-11 19:41:41 +0530

    Iplimented functionality to add new countries

--- src/Filter.js
+++ src/Filter.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Filter extends Component {
   state = {
-    singleCountry: "",
+    newCountry: "",
     countries: [
       { id: 1, name: "Algeria" },
       { id: 2, name: "Australia" },
@@ -19,24 +19,25 @@ class Filter extends Component {
     ]
   };
 
-  // newcontry = e => {
-  //   {
-  //     this.setState({
-  //       singleCountry: e.target.value
-  //     });
-  //   }
-
-  //   addCountry = e => {
-  //     const countries = this.state.countries.map(item => {
-  //       return item;
-  //     });
-
-  //     (countries = countries), this.state.singleCountry;
-  //     console.log("........", countries);
-  //     this.setState({ countries });
-  //   };
-  // };
-
+  newcontry = e => {
+    {
+      this.setState({
+        newCountry: e.target.value
+      });
+    }
+    console.log("new country", this.state.newCountry);
+  };
+  addCountry = country => {
+    const newCountry = {
+      id: this.state.countries.length + 1,
+      name: this.state.newCountry
+    };
+    const countries = [...this.state.countries, newCountry];
+    console.log("I am adding new counrty", countries);
+    this.setState({
+      countries: countries
+    });
+  };
   handlechange = e => {
     const newList = this.state.countries.filter(item => {
       if (item.name.indexOf(e.target.value) > -1) {
@@ -49,12 +50,27 @@ class Filter extends Component {
     });
   };
 
+  allcountry = item => {
+    const allCountry = this.state.countries.map(country => {
+      return country;
+    });
+  };
+
   render() {
     const countryList = this.state.filtedCounties.map(item => {
       return (
         <div key={item.id}>
           <ul>
             <li>{item.name}</li>
+          </ul>
+        </div>
+      );
+    });
+    const allCountry = this.state.countries.map(country => {
+      return (
+        <div key={country.id}>
+          <ul>
+            <li>{country.name}</li>
           </ul>
         </div>
       );
@@ -89,6 +105,18 @@ class Filter extends Component {
             onChange={this.newcontry}
           />
           <button onClick={this.addCountry}>Submit</button>
+        </div>
+        <div>
+          <table id="myTable">
+            <tbody>
+              <tr className="header">
+                <th>Name</th>
+              </tr>
+              <tr>
+                <td>{allCountry}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     );
